3f1af05d193e9f057d05e5d2f52b5c3fb296c4c2
Mallory Hancock <malloryhancock89@gmail.com>
2019-09-04 09:32:14 -0700

    add basic database models

--- quotemeal/server.py
+++ quotemeal/server.py
@@ -1,2 +1,20 @@
+from datetime import datetime
+
 import flask
 from peewee import *
+
+db = SqliteDatabase('quotes.db')
+
+class QuotemealMeta(Model):
+    class Meta:
+        database = db
+
+class User(QuotemealMeta):
+    name = CharField()
+
+class Quote(QuotemealMeta):
+    text = TextField()
+    submitted = DateTimeField(default=datetime.now)
+    author = ForeignKeyField(User, backref='quotes')
+
+db.create_tables([User, Quote])
